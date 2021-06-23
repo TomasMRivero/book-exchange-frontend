01d21e8361e25e7294ca9ba6119abae2cc5ef0af
Tom <tmartinezrivero@alumno.unlam.edu.ar>
2021-06-23 15:18:18 -0300

    arreglar Nav
    Se arregla nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,27 @@
 'use strict'
-import './css/Nav.css';
 import { AppBar, Toolbar, IconButton, Slide, useScrollTrigger } from '@material-ui/core';
 import { Home, AccountCircle  } from '@material-ui/icons';
 import { useHistory, useRouteMatch } from "react-router";
 import SearchField from "./SearchField";
 import { useCallback } from 'react';
+import { fade, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+        grow: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        search: {
+            color: 'inherit',
+            width: '100%',
+        },
+        iconContaier:{
+            display: 'flex',
+            justifyContent: 'flex-end',
+        }
+}));
 
 function ShowSearchBar({match}) {
     if (!match.isExact){
@@ -14,30 +31,35 @@ function ShowSearchBar({match}) {
 }
 
 export default function Nav(){
+    const classes = useStyles();
     const history = useHistory();
     const match = useRouteMatch("/");
     const onClickHome = useCallback((e) => {
         e.preventDefault()
         history.push('/');
     })
-    let classes = ['nav-bar'];
+    /*let classes = ['nav-bar'];
     if (match.isExact){
         classes.push("no-searchbar");
-    }
+    }*/
     return(
-        <div className={classes.join(' ')}>
-            <AppBar position="fixed">
-                <Toolbar>
+        <div className={classes.grow}>
+            <AppBar position="sticky">
+                <Toolbar style={{display: 'flex', justifyContent:'space-between'}}>
                     <IconButton
+                        edge="start"
+                        className={classes.menuButton}
                         className="home-button" 
                         onClick = {onClickHome}
                     >
                         <Home />
                     </IconButton>
-                    <ShowSearchBar match = {match}/>
-                    <IconButton edge="end" className = "user-icon">
-                        <AccountCircle />
-                    </IconButton>
+                    <ShowSearchBar className={classes.search} nav={1} match = {match} style={{width:'50vw'}}/>
+                    <div className={classes.iconContaier}>
+                        <IconButton edge="end" className = {classes.userIcon}>
+                            <AccountCircle />
+                        </IconButton>   
+                    </div>
                 </Toolbar>
             </AppBar>
         </div>
--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,8 +1,64 @@
 import { useState, useCallback } from "react";
 import { useHistory } from 'react-router';
-import {Select, InputBase, MenuItem, IconButton} from '@material-ui/core';
+import {Select, InputBase, MenuItem, IconButton, Grid} from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-export default function SearchField(){
+
+import { fade, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    search: {
+        width: '70vw',
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        display: 'flex',
+        position: 'relative',
+        padding: '5px',
+        height: '1.7em',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+    },
+    IconButton:{
+        padding: theme.spacing(0),
+        height: '100%',
+        margin: 0,
+        position: 'absolute',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+      height: '100%',
+      display: 'flex',
+      flexGrow: 1,
+      alignItems: 'center',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      paddingLeft: `calc(1em + ${theme.spacing(2)}px)`,
+      width: '100%'
+    },
+    selectContainer:{
+        height: '100%',
+        display: 'flex',
+        flexGrow: 1,
+        justifyContent: 'flex-end',
+    },
+    select:{
+        height: '100%',
+        fontSize: 14
+    }
+}));
+
+export default function SearchField(props){
+    const classes = useStyles();
     const history = useHistory();
 
     const [searchValue, setSearchValue] = useState('');
@@ -30,45 +86,42 @@ export default function SearchField(){
     
     return(
         <>
-            <form className="search-form" onSubmit={onSearch} > 
-                <IconButton
-                    type="submit"
-                    className="search-button"
-                >
-                    <SearchIcon />   
-                </IconButton>
-                <InputBase
-                    className = "search-value"
-                    placeholder = "Buscar por..."
-                    onChange={onChangeSearchValue}
-                    value={searchValue}
-                />
-                <Select
-                    className="search-field"
-                    id="field"
-                    name="field"
-                    form="search-form"
-                    onChange={onChangeSearchField}
-                    value={searchField}
-                >
-                    <MenuItem
-                        key="title"
-                        value="title"
-                    >
-                        Título
-                    </MenuItem>
-                    <MenuItem
-                        key="author"
-                        value="author"
-                    >
-                        Autor
-                    </MenuItem>
-                </Select>
-                
-                
-            
-                
-
+            <form className={classes.search} onSubmit={onSearch} >
+                    <IconButton type='submit' className={classes.IconButton}>
+                        <SearchIcon className={classes.searchIcon}/>
+                    </IconButton>
+                    <div className={classes.inputRoot}>
+                        <InputBase 
+                            className= {classes.inputInput}
+                            placeholder = "Buscar por..."
+                            onChange={onChangeSearchValue}
+                            value={searchValue}
+                        />
+                        <div className={classes.selectContainer}>
+                        <Select
+                            className = {classes.select}
+                            variant="outlined"
+                            id="field"
+                            name="field"
+                            form="search-form"
+                            onChange={onChangeSearchField}
+                            value={searchField}
+                        >
+                            <MenuItem
+                                key="title"
+                                value="title"
+                            >
+                                Título
+                            </MenuItem>
+                            <MenuItem
+                                key="author"
+                                value="author"
+                            >
+                                Autor
+                            </MenuItem>
+                        </Select>
+                        </div>
+                    </div>
             </form>
         </>
     )
